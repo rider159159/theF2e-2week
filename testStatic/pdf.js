d06b21e2a164249fceb9ea76a5196039726f073d
Ryder <rider159159@gmail.com>
2022-10-21 18:13:44 +0800

    updata

---
+++ testStatic/pdf.js
@@ -0,0 +1,143 @@
+const Base64Prefix = "data:application/pdf;base64,";
+const add = document.querySelector(".add");
+pdfjsLib.GlobalWorkerOptions.workerSrc = "https://mozilla.github.io/pdf.js/build/pdf.worker.js";
+const sign = document.querySelector(".sign");
+const deleteIcon = "data:image/svg+xml,%3C%3Fxml version='1.0' encoding='utf-8'%3F%3E%3C!DOCTYPE svg PUBLIC '-//W3C//DTD SVG 1.1//EN' 'http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd'%3E%3Csvg version='1.1' id='Ebene_1' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' x='0px' y='0px' width='595.275px' height='595.275px' viewBox='200 215 230 470' xml:space='preserve'%3E%3Ccircle style='fill:%23F44336;' cx='299.76' cy='439.067' r='218.516'/%3E%3Cg%3E%3Crect x='267.162' y='307.978' transform='matrix(0.7071 -0.7071 0.7071 0.7071 -222.6202 340.6915)' style='fill:white;' width='65.545' height='262.18'/%3E%3Crect x='266.988' y='308.153' transform='matrix(0.7071 0.7071 -0.7071 0.7071 398.3889 -83.3116)' style='fill:white;' width='65.544' height='262.179'/%3E%3C/g%3E%3C/svg%3E";
+const img = document.createElement('img');
+img.src = deleteIcon;
+
+// 若 localStorage 有資料才放入
+if (localStorage.getItem("signImage")) {
+  sign.src = localStorage.getItem("signImage");
+}
+
+// 使用原生 FileReader 轉檔
+function readBlob(blob) {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.addEventListener("load", () => resolve(reader.result));
+    reader.addEventListener("error", reject);
+    reader.readAsDataURL(blob);
+  });
+}
+
+async function printPDF(pdfData) {
+
+  // 將檔案處理成 base64
+  pdfData = await readBlob(pdfData);
+
+  // 將 base64 中的前綴刪去，並進行解碼
+  const data = atob(pdfData.substring(Base64Prefix.length));
+
+  // 利用解碼的檔案，載入 PDF 檔及第一頁
+  const pdfDoc = await pdfjsLib.getDocument({ data }).promise;
+  const pdfPage = await pdfDoc.getPage(1);
+
+  // 設定尺寸及產生 canvas
+  const viewport = pdfPage.getViewport({ scale: window.devicePixelRatio });
+  const canvas = document.createElement("canvas");
+  const context = canvas.getContext("2d");
+
+  // 設定 PDF 所要顯示的寬高及渲染
+  canvas.height = viewport.height;
+  canvas.width = viewport.width;
+  const renderContext = {
+    canvasContext: context,
+    viewport,
+  };
+  const renderTask = pdfPage.render(renderContext);
+
+  // 回傳做好的 PDF canvas
+  return renderTask.promise.then(() => canvas);
+}
+
+async function pdfToImage(pdfData) {
+
+  // 設定 PDF 轉為圖片時的比例
+  const scale = 1 / window.devicePixelRatio;
+
+  // 回傳圖片
+  return new fabric.Image(pdfData, {
+    id: "renderPDF",
+    scaleX: scale,
+    scaleY: scale,
+  });
+}
+
+// 此處 canvas 套用 fabric.js
+const canvas = new fabric.Canvas("canvas");
+
+document.querySelector("input").addEventListener("change", async (e) => {
+  canvas.requestRenderAll();
+  const pdfData = await printPDF(e.target.files[0]);
+  const pdfImage = await pdfToImage(pdfData);
+
+  // 透過比例設定 canvas 尺寸
+  canvas.setWidth(pdfImage.width / window.devicePixelRatio);
+  canvas.setHeight(pdfImage.height / window.devicePixelRatio);
+
+  // 將 PDF 畫面設定為背景
+  canvas.setBackgroundImage(pdfImage, canvas.renderAll.bind(canvas));
+});
+
+// canvas 出現簽名
+sign.addEventListener("click", () => {
+  if (!sign.src) return;
+
+  fabric.Image.fromURL(sign.src, function (image) {
+    // 設定簽名出現的位置及大小，後續可調整
+    image.top = 400;
+    image.scaleX = 0.5;
+    image.scaleY = 0.5;
+    canvas.add(image);
+    canvas.setActiveObject(image);
+  });
+});
+
+// // 引入套件所提供的物件
+const pdf = new jsPDF();
+
+const download = document.querySelector(".download");
+
+download.addEventListener("click", () => {
+
+  // 將 canvas 存為圖片
+  const image = canvas.toDataURL("image/png");
+  
+  // 設定背景在 PDF 中的位置及大小
+  const width = pdf.internal.pageSize.width;
+  const height = pdf.internal.pageSize.height;
+  pdf.addImage(image, "png", 0, 0, width, height);
+
+  // 將檔案取名並下載
+  pdf.save("download.pdf");
+});
+fabric.Object.prototype.transparentCorners = false;
+fabric.Object.prototype.cornerColor = 'blue';
+fabric.Object.prototype.cornerStyle = 'circle';
+// 刪除按鈕
+fabric.Object.prototype.controls.deleteControl = new fabric.Control({
+  x: 0.5,
+  y: -0.5,
+  offsetY: 16,
+  cursorStyle: 'pointer',
+  mouseUpHandler: deleteObject,
+  render: renderIcon,
+  cornerSize: 24
+});
+
+function deleteObject(eventData, transform) {
+  var target = transform.target;
+  var canvas = target.canvas;
+      canvas.remove(target);
+      canvas.requestRenderAll();
+}
+
+function renderIcon(ctx, left, top, styleOverride, fabricObject) {
+  var size = this.cornerSize;
+  ctx.save();
+  ctx.translate(left, top);
+  ctx.rotate(fabric.util.degreesToRadians(fabricObject.angle));
+  ctx.drawImage(img, -size/2, -size/2, size, size);
+  ctx.restore();
+}
