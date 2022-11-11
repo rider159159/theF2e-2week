61707048a1c9ca6f8a9380b521eb52dd451570ea
Ryder <rider159159@gmail.com>
2022-11-11 19:37:53 +0800

    updata

--- src/pages/step2.vue
+++ src/pages/step2.vue
@@ -112,6 +112,7 @@
       cornerSize: 24
     });
   }
+
   function deleteObject(eventData, transform) {
     const target = transform.target;
     const canvas = target.canvas;
@@ -148,7 +149,7 @@
 
     // 將 canvas 存為圖片
     const image = canvas.toDataURL("image/png");
-    
+    console.log(image)
     // 設定背景在 PDF 中的位置及大小
     const width = pdf.internal.pageSize.width;
     const height = pdf.internal.pageSize.height;
